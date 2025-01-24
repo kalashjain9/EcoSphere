3cd519871c52f2d87de9e9a2890c55622b781faa
jaimeenbhagat <jaimeenbhagat@outlook.com>
2025-01-24 20:00:30 +0530

    Added Carbon Footprinting code

--- energy_monitor/utils/calculator.py
+++
@@ -1,9 +0,0 @@
-def calculate_carbon_footprint(electricity, gas, water):
-    electricity_factor = 0.92  # kg CO2/kWh
-    gas_factor = 2.3          # kg CO2/liter
-    water_factor = 0.36       # kg CO2/liter
-    
-    total = (electricity * electricity_factor +
-             gas * gas_factor +
-             water * water_factor)
-    return round(total, 2)
